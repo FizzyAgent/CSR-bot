300e72f0cedca026bf899790d8277e189eedb624
Pinxi <tpinxi@gmail.com>
2023-08-17 19:36:10 +0800

    use streamlit chat input

--- demo_app.py
+++ demo_app.py
@@ -3,7 +3,7 @@ from iso3166 import countries
 
 from api.driver.logic_service import run
 from api.models.messages import Role, Message
-from app.rendering import render_left_message, render_right_message, render_style
+from app.rendering import render_left_message, render_style, render_right_message
 from app.states import (
     init_states,
     get_chat_messages,
@@ -23,7 +23,7 @@ st.set_page_config(page_title="CSR Bot Demo", layout="wide")
 
 with st.sidebar:
     left, _, mid = st.columns((2, 0.1, 3))
-right = st.columns((1))[0]
+right = st.container()
 
 render_style()
 init_states()
@@ -83,25 +83,19 @@ with right:
             render_right_message(delta=st.container(), message=message)
         elif message.role == Role.bot:
             render_left_message(delta=st.container(), message=message)
-    new_input = st.text_area(
-        label="Message Box",
-        label_visibility="hidden",
-        key=f"message_input_{len(chat_messages)}",
-    )
-
-    def save_user_input() -> None:
-        user_input = new_input.strip()
-        if len(user_input) == 0:
-            return
-        new_message = Message(role=Role.customer, text=user_input)
-        save_customer_message(message=new_message)
 
     has_openai_key = len(openai_key) > 0
-    st.button("Submit", disabled=not has_openai_key, on_click=save_user_input)
+    user_input = st.chat_input(
+        placeholder="Type your message here", max_chars=200, disabled=not has_openai_key
+    )
     if not has_openai_key:
         st.markdown(
             "Please enter your OpenAI key under Settings to start chatting with CSR Bot!"
         )
+
+    if user_input is not None and len((user_input := user_input.strip())) > 0:
+        new_message = Message(role=Role.customer, text=user_input)
+        save_customer_message(message=new_message)
 
 terminated = False
 messages = get_chat_messages()
